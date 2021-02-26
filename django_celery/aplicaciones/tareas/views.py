caf6086bd6499f91bb6d5de19475769bc4a3825a
Emi <emazzurque@gmail.com>
2021-02-26 19:52:09 -0300

    first commit

---
+++ django_celery/aplicaciones/tareas/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+import string
+from django.contrib.auth.models import User
+from django.utils.crypto import get_random_string
+
+def create_user_random(cantidad):
+    for x in range(cantidad):
+        username = "usuario_{}".format(get_random_string(5,string.ascii_letters))
+        email =  "{}@miempresa.ar".format(username)
+        password = get_random_string(50)
+        User.objects.create_user(username=username, email=email,password=password)
+    return "{} Usuario creados correctamente..!".format(x)
